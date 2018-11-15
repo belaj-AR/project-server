28e2a8bb51bf17c53a2b4f708b4af641800887ee
pputra <mynameisyoga@live.com>
2018-11-15 13:25:28 +0700

    add middleware for item crud

--- main-server/routes/items.js
+++ main-server/routes/items.js
@@ -1,6 +1,10 @@
-const router = require('express').Router()
-const Controller = require('../controllers/itemController')
+const router = require('express').Router();
+const Controller = require('../controllers/itemController');
+const { isLogin, getCRUDToken } = require('../middlewares');
 
+
+//router.use(isLogin);
+router.use(getCRUDToken);
 router.get('/', Controller.getAll);
 router.get('/:id', Controller.getOne);
 router.put('/:id', Controller.updateItem);
